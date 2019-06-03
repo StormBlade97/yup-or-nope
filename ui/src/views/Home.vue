45fc91d12dbc27d277f40c69a5ce3eb0310f5bca
Thanh Nguyen <thanh.nguyen@zalando.fi>
2019-06-03 14:53:01 +0300

    Bootstrap front-end

---
+++ ui/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <div class="section">
+      <div class="container">
+        <p class="title">Hello World</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
